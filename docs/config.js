86fea33cfd523ba3705788fb9c4baac97c589795
Michael Huang <cj2vum4@gmail.com>
2026-07-03 17:03:48 +0800

    URL

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 window.IPAS_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://nmmowmggcnnnrsysaohx.supabase.co/rest/v1/",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tbW93bWdnY25ubnJzeXNhb2h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjI3NjIsImV4cCI6MjA5ODYzODc2Mn0.yoqtcYJFlcuI3byiGOQfddHy2b9xNRP6o4_FDrzdR-M",
 };
